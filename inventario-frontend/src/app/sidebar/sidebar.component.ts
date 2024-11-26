ca58bd1eb4bcfe08f0faef137771cfb65ab5b464
KEVIN ALONSO MAMANI CONDORI <kmamanicondo@unsa.edu.pe>
2024-11-26 12:44:39 -0500

    feat: se agrego el auth al login

--- inventario-frontend/src/app/sidebar/sidebar.component.ts
+++ inventario-frontend/src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,5 +11,15 @@ import { RouterModule } from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  constructor(
+    private authService: AuthService,  // Inyectamos AuthService
+    private router: Router             // Inyectamos Router para redirigir
+  ) {}
+
+  onLogout() {
+    this.authService.logout(); // Llamamos al método logout del AuthService
+    this.router.navigate(['/login']); // Redirigimos al usuario a la página de login
+  }
 
 }
